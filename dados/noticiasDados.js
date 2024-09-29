a89026f3fa2b6ab94cfd6f523e08d489c2d49887
ERNALDO VIEIRA <69253807+ernaldo777@users.noreply.github.com>
2024-09-28 21:21:52 -0300

    Add files via upload

---
+++ dados/noticiasDados.js
@@ -0,0 +1,16 @@
+
+const noticiasDados = [
+  {
+    id: '1',
+    titulo: 'Noticias 1',
+    conteudo: 'Conteudo das noticias 1',
+  },
+  {
+    id: '2',
+    titulo: 'Noticias 2',
+    conteudo: 'Conteudo das noticias 2',
+  },
+  // Podemos adicionar mais notícias aqui
+];
+
+export default noticiasDados;
